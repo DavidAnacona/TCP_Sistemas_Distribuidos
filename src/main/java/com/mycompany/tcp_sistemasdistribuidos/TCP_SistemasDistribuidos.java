49047a9c6a0c333ae8056f0b4b7767e25f251723
DavidAnacona <danaconac@ucentral.edu.co>
2024-08-20 23:42:33 -0500

    App utilizando TCP para enviar mensajes a multiples clientes a la vez

---
+++ src/main/java/com/mycompany/tcp_sistemasdistribuidos/TCP_SistemasDistribuidos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tcp_sistemasdistribuidos;
+
+/**
+ *
+ * @author davii
+ */
+public class TCP_SistemasDistribuidos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
